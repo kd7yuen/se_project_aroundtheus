84e97d038e61a953bb4aec319fa5e3cb90c436cd
Kevin Yuen <kd7yuen@gmail.com>
2023-02-14 10:37:56 -0800

    New update for my project

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+let initialCards = [
+  {
+    name: "amsterdam",
+    link: url("https://unsplash.com/photos/-e4vLFZV9QM"),
+  },
+  {
+    name: "heathrowAirport",
+    link: url("https://unsplash.com/photos/EI3lexoBY60"),
+  },
+  {
+    name: "newYorkCity",
+    link: url("https://unsplash.com/photos/l8ypMiU1Hio"),
+  },
+  {
+    name: "rijksmuseum",
+    link: url("https://unsplash.com/photos/ktIeizXobW8"),
+  },
+  {
+    name: "copenhagen",
+    link: url("https://unsplash.com/photos/yGFdovJATMc"),
+  },
+  {
+    name: "applePark",
+    link: url("https://unsplash.com/photos/0HfJkC-Udhg"),
+  },
+];
