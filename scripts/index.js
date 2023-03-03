3775cc9b637dc71885cda8a658712436ad040557
Kevin Yuen <kd7yuen@gmail.com>
2023-03-02 19:53:12 -0800

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,76 @@
 const initialCards = [
   {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+    name: "Amsterdam",
+    link: "https://images.unsplash.com/photo-1584003564911-a7a321c84e1c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1884&q=80",
   },
   {
-    name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+    name: "Heathrow Airport",
+    link: "https://images.unsplash.com/photo-1585587156830-670bd420de68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
   },
   {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+    name: "New York City",
+    link: "https://images.unsplash.com/photo-1602940659805-770d1b3b9911?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80",
   },
   {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+    name: "Rijksmuseum",
+    link: "https://images.unsplash.com/photo-1589825743636-cd96373c3319?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
   },
   {
-    name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+    name: "Copenhagen",
+    link: "https://images.unsplash.com/photo-1557846978-e801fff1c245?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
   },
   {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+    name: "Apple Park",
+    link: "https://images.unsplash.com/photo-1591292784843-aa35ebb7897b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80",
   },
 ];
+function GetCardElement(data) {
+  const template = document.querySelector("#cardsList");
+  const lists = document.querySelector(".cards");
+  const cardImage = template.querySelector(".cards__image");
+  const cardTitle = template.querySelector(".cards__title");
 
-var profileModalOpen = document.getElementById("profile-open");
-var profileModalClose = document.getElementById("profile-close");
+  initialCards.forEach((data) => {
+    let cardElement = template.content.cloneNode(true);
+    cardImage.src = data.link;
+    cardTitle.textContent = data.name;
+    cardImage.alt = data.name;
+    lists.append(cardElement);
+  });
+}
+GetCardElement();
 
-profileModalOpen.addEventListener("click", openFunction);
-function openFunction() {
-  document.getElementById("modal__container").style.display = "flex";
+//container
+const modal = document.querySelector(".modal");
+const profileEditForm = document.querySelector(".modal__form");
+
+//Buttons
+const profileEditButton = document.querySelector(".profile__edit");
+const modalCloseButton = document.querySelector(".modal__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+//form data
+const nameInput = profileEditForm.querySelector(".modal__name");
+const jobInput = profileEditForm.querySelector(".modal__description");
+
+function closeModal() {
+  modal.classList.remove("modal__content-open");
 }
 
-profileModalClose.addEventListener("click", closeFunction);
-function closeFunction() {
-  document.getElementById("modal__container").style.display = "none";
+function openModal() {
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileDescription.textContent;
+  modal.classList.add("modal__content-open");
 }
+
+profileEditButton.addEventListener("click", openModal);
+modalCloseButton.addEventListener("click", closeModal);
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+  closeModal();
+}
+profileEditForm.addEventListener("submit", handleProfileFormSubmit);
