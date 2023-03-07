b91f305494587a8f3f40acfcec18fd9151c7f45a
Kevin Yuen <kd7yuen@gmail.com>
2023-03-06 18:20:02 -0800

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -26,23 +26,30 @@ const initialCards = [
   },
 ];
 
-//grab card elements to be repeated throughout array
-function getCardElement(data) {
-  const template = document.querySelector("#cardsList").content.querySelector(".cards__block");
-  const cardElement = template.cloneNode(true);
-  const lists = document.querySelector(".cards");
-  const cardImage = cardElement.querySelector(".cards__image");
-  const cardTitle = cardElement.querySelector(".cards__title");
-
-  cardImage.src = data.link;
-  cardTitle.textContent = data.name;
-  cardImage.alt = data.name;
-  lists.append(cardElement);
-}
+    const lists = document.querySelector(".cards");
+    const template = document.querySelector("#cardsList").content.querySelector(".cards__block");
 
 initialCards.forEach((card) => {
   getCardElement(card);
+
+  //Grab data that can be reused
+  function getCardElement(data) {
+    const cardElement = template.cloneNode(true);
+    const cardImage = cardElement.querySelector(".cards__image");
+    const cardTitle = cardElement.querySelector(".cards__title");
+    cardImage.src = data.link;
+    cardTitle.textContent = data.name;
+    cardImage.alt = data.name;
+    lists.append(cardElement);
+  }
 });
+
+
+
+
+
+
+
 
 //container for modal and modal's form
 const modal = document.querySelector(".modal");
@@ -55,19 +62,19 @@ const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
 
 //form data
-const nameInput = profileEditForm.querySelector(".modal__name");
-const jobInput = profileEditForm.querySelector(".modal__description");
+const nameInput = profileEditForm.querySelector(".modal__input_type_name");
+const jobInput = profileEditForm.querySelector(".modal__input_type_description");
 
 //removes class
 function closeModal() {
-  modal.classList.remove("modal__content-open");
+  modal.classList.remove("modal-open");
 }
 
 //adds class
 function openModal() {
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileDescription.textContent;
-  modal.classList.add("modal__content-open");
+  modal.classList.add("modal-open");
 }
 
 //records for when edit button is clicked
@@ -81,3 +88,15 @@ function handleProfileFormSubmit(evt) {
   closeModal();
 }
 profileEditForm.addEventListener("submit", handleProfileFormSubmit);
+
+
+// Declare a getCardElement() function with one parameter named data. You’ll be passing objects of the array to it. The function should:
+// clone the template element with all its content and store it in a cardElement variable
+// access the card title and image and store them in variables
+// set the path to the image to the link field of the object
+// set the image alt text to the name field of the object
+// set the card title to the name field of the object, too
+// return the ready HTML element with the filled-in data
+// Iterate over the cards array using a loop, and in each iteration:
+// Run your getCardElement() function on the card object to create the HTML element.
+// Use the appropriate built-in DOM method to add this HTML element to the page.
